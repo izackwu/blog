5771c22dab11e1d7451b3f7cb22e8e61775ac680
Keith Null <keith1126@126.com>
2020-09-22 13:12:44 +0800

    Add plugin for redirection

--- gatsby-config.js
+++ gatsby-config.js
@@ -165,6 +165,8 @@ module.exports = {
         },
       },
     },
+    "gatsby-redirect-from",
+    "gatsby-plugin-meta-redirect", // make sure this is always the last one
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
